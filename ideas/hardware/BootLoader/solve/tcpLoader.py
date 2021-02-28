c901448a50f7a6930b63187b65dd6ffb79c15b62
MalumK <kostya6.kostya@yandex.ru>
2021-02-28 11:22:32 +0300

    add solve

--- ideas/hardware/BootLoader/solve/tcpLoader.py
+++ ideas/hardware/BootLoader/solve/tcpLoader.py
@@ -1,9 +1,9 @@
 import socket
-
+import time
+from time import sleep
 conn = socket.socket()
-conn.connect(("localhost", 8686))
-
-a = open('/home/anon/code.bin', 'rb')
+conn.connect(("localhost", 38686))
+a = open('code.bin', 'rb')
 step = 0
 f = 0
 f1 = 1
@@ -14,31 +14,34 @@ while(1):
 	print(serialString)
 	# Print the contents of the serial data
 	if(step == 0):
-		conn.send(b"r")
+		conn.sendall(b"r") 
 		step += 1
 		continue
+	if(serialString == b'g'):
+		continue
 	if(serialString == b'c'):
-		if(step > 0x1000):
-			conn.send(b'R')
+		if(step > 0x200):
+			conn.sendall(b'R')
 			while(1):
 				serialString = conn.recv(1)
 				print(serialString)
 			exit(0)
 		if(f1 == 1):
-			conn.send(b'\x00')	
+			conn.sendall(b'\x00')	
 		else:
-			conn.send(b'\x80')
+			conn.sendall(b'\x80')
 			f1 = 1
 		continue
 	if(not f):
 		try:
 			s = a.read(1)
 			#if(step%3 != 1):
-			conn.send(s)
+			conn.sendall(s)
 		except:
 			f = 1
-			conn.send(b"\x00")
+			conn.sendall(b"\x00")
 	step += 1
+	#time.sleep(0.5	)
 	#if(step == 29000):
 	#	exit(0)
 	# exit
